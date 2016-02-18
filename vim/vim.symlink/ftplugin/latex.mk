d8f67d3f6cf2a6e9d494793ebe2e66ccd92f8d1b
Per Magnus Veierland <pveierland@gmail.com>
2016-02-18 15:08:42 +0100

    vim/latex/bash: Updated build configuration

--- vim/vim.symlink/ftplugin/latex.mk
+++ vim/vim.symlink/ftplugin/latex.mk
@@ -1,9 +1,8 @@
 .PHONY: clean
 
 %.pdf: %.tex $(DEPENDS)
-	cp *.tex /media/sf_ubuntu/
-	cp ~/permve-ntnu-latex/*.tex /media/sf_ubuntu/
-	cd /media/sf_ubuntu/ && pdflatex --shell-escape $<
+	pdflatex --shell_escape $<
+	cp *.pdf /media/sf_ubuntu/
 
 clean:
 	rm -rf *.aux *.bbl *.blg *.log *.toc *.snm *.out *.nav tags
