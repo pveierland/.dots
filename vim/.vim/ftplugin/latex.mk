e14b48ad17297a77c33c3c1268b0303dfe162eed
Per Magnus Veierland <pveierland@gmail.com>
2016-10-06 12:27:56 +0200

    vim/latex: Added test to sf_ubuntu copy

--- vim/.vim/ftplugin/latex.mk
+++ vim/.vim/ftplugin/latex.mk
@@ -2,7 +2,7 @@
 
 %.pdf: %.tex $(DEPENDS)
 	latexmk -pdf -pdflatex="pdflatex --shell-escape %O %S" $<
-	cp *.pdf /media/sf_ubuntu/
+	if [ -e /media/sf_ubuntu/ ]; then cp *.pdf /media/sf_ubuntu/; fi;
 
 clean:
 	rm -rf *.aux *.bbl *.blg *.log *.toc *.snm *.out *.nav tags
